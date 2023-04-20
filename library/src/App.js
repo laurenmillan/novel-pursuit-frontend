7998e7ef0a600e5e1eebf139ddf855a4e610c10b
Lauren Millan <laurenmillan@mydsn.net>
2023-04-20 15:02:03 -0400

    add dynamic navbar and successful routing

--- library/src/App.js
+++ library/src/App.js
@@ -11,16 +11,21 @@ import Bookmarks from './Components/Bookmarks';
 import NotFound from './404/404';
 // import SearchForm from './books/SearchForm';
 
+import NavBar from './Components/NavBar';
+
 const App = () => {
 	return (
-		<Routes>
-			<Route exact path="/" element={<Home />} />
-			<Route exact path="/login" element={<Login />} />
-			<Route exact path="/signup" element={<Signup />} />
-			<Route exact path="/profile" element={<Profile />} />
-			<Route exact path="/bookmarks" element={<Bookmarks />} />
-			<Route path="/*" element={<NotFound />} />
-		</Routes>
+		<div className="App">
+			<NavBar />
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+				<Route exact path="/login" element={<Login />} />
+				<Route exact path="/signup" element={<Signup />} />
+				<Route exact path="/profile" element={<Profile />} />
+				<Route exact path="/bookmarks" element={<Bookmarks />} />
+				<Route path="/*" element={<NotFound />} />
+			</Routes>
+		</div>
 	);
 };
 
---
+++ library/src/Components/NavBar.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './NavBar.css';
+import { NavLink } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
+
+/** Displays dynamic navbar components. */
+
+function NavBar() {
+	return (
+		<div>
+			<Navbar bg="dark" expand="md" className="navbar-dark">
+				<Navbar.Brand as={NavLink} to="/">
+					Library
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+					<Nav className="ml-auto" navbar>
+						<Nav.Item>
+							<Nav.Link as={NavLink} to="/login">
+								Log in
+							</Nav.Link>
+						</Nav.Item>
+
+						<Nav.Item>
+							<Nav.Link as={NavLink} to="/signup">
+								Sign Up
+							</Nav.Link>
+						</Nav.Item>
+
+						{/* TODO: display navbar to show Bookmarks link only on login */}
+						<Nav.Item>
+							<Nav.Link as={NavLink} activeclassname="active" to="/bookmarks">
+								Bookmarks
+							</Nav.Link>
+						</Nav.Item>
+
+						<Nav.Item>
+							<Nav.Link as={NavLink} activeclassname="active" to="/profile">
+								Profile
+							</Nav.Link>
+						</Nav.Item>
+
+						<Nav.Item>
+							<Nav.Link as={NavLink} activeclassname="active" to="/">
+								Log out
+							</Nav.Link>
+						</Nav.Item>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+		</div>
+	);
+}
+
+export default NavBar;
