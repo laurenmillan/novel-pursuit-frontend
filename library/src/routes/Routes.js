2ff834fba4811d5d0318cc36ca1655fce857ab2f
Lauren Millan <laurenmillan@mydsn.net>
2023-04-24 17:49:21 -0400

    Remove 404 page and use Navigate to redirect user to root path

--- library/src/routes/Routes.js
+++ library/src/routes/Routes.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 import Home from './Components/Home';
 import Login from './Components/LoginForm';
 import Signup from './Components/SignupForm';
 import Profile from './Components/ProfileForm';
 import Bookmarks from './Components/Bookmarks';
-import NotFound from './404/404';
 
 /** Routing Logic. */
 
@@ -18,7 +17,7 @@ const AppRoutes = () => {
 			<Route exact path="/signup" element={<Signup />} />
 			<Route exact path="/profile" element={<Profile />} />
 			<Route exact path="/bookmarks" element={<Bookmarks />} />
-			<Route path="/*" element={<NotFound />} />
+			<Route path="/*" element={<Navigate to="/" />} />
 		</Routes>
 	);
 };
