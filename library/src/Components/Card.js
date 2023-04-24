14a99ccaa3caa46b0ef407d8faa2231f62ed65f3
Lauren Millan <laurenmillan@mydsn.net>
2023-04-24 13:19:59 -0400

    add book title and author to Card, still need to figure out how to load cover_url

--- library/src/Components/Card.js
+++ library/src/Components/Card.js
@@ -16,4 +16,23 @@ const Card = ({ book }) => {
 	);
 };
 
+// return (
+// 		<React.Fragment>
+// 			{book.map((key) => {
+// 				// let thumbnail = key.cover_i
+// 				return (
+// 					<React.Fragment>
+// 						<div className="card">
+// 							<img src={thumbnail} alt={book.title} />
+// 							<div className="bottom">
+// 								<h3 className="title">{book.title}</h3>
+// 								<h2 className="author">{book.author_name}</h2>
+// 							</div>
+// 						</div>
+// 					</React.Fragment>
+// 				);
+// 			})}
+// 		</React.Fragment>
+// 	);
+
 export default Card;
