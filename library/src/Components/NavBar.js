aae81a60eb15adfd1a89853552f17ac76a463b3f
Lauren Millan <laurenmillan@mydsn.net>
2023-04-25 08:41:53 -0400

    Add login and logout functionality

--- library/src/Components/NavBar.js
+++ library/src/Components/NavBar.js
@@ -3,9 +3,15 @@ import './NavBar.css';
 import { NavLink } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
 
-/** Displays dynamic navbar components. */
+/** Displays dynamic navbar components.
+ * 
+ * - props: user and logout, passed down from the App component.
+ * - user represents current user.
+ * - logout is a function that logs out the user when called.
+ * 
+*/
 
-function NavBar() {
+const NavBar = ({ user, logout }) => {
 	return (
 		<div>
 			<Navbar expand="md" className="custom-navbar">
@@ -15,41 +21,48 @@ function NavBar() {
 				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 				<Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
 					<Nav className="ml-auto" navbar>
-						<Nav.Item>
-							<Nav.Link as={NavLink} to="/login">
-								Log in
-							</Nav.Link>
-						</Nav.Item>
+						{/* if user is not logged in, render /login, /signup */}
+						{!user ? (
+							<React.Fragment>
+								<Nav.Item>
+									<Nav.Link as={NavLink} to="/login">
+										Log in
+									</Nav.Link>
+								</Nav.Item>
 
-						<Nav.Item>
-							<Nav.Link as={NavLink} to="/signup">
-								Sign Up
-							</Nav.Link>
-						</Nav.Item>
+								<Nav.Item>
+									<Nav.Link as={NavLink} to="/signup">
+										Sign Up
+									</Nav.Link>
+								</Nav.Item>
+							</React.Fragment>
+						) : (
+							// if a user is logged in, show /profile, /bookmarks
+							<React.Fragment>
+								<Nav.Item>
+									<Nav.Link as={NavLink} activeclassname="active" to="/bookmarks">
+										Bookmarks
+									</Nav.Link>
+								</Nav.Item>
 
-						{/* TODO: display navbar to show Bookmarks link only on login */}
-						<Nav.Item>
-							<Nav.Link as={NavLink} activeclassname="active" to="/bookmarks">
-								Bookmarks
-							</Nav.Link>
-						</Nav.Item>
+								<Nav.Item>
+									<Nav.Link as={NavLink} activeclassname="active" to="/profile">
+										Profile
+									</Nav.Link>
+								</Nav.Item>
 
-						<Nav.Item>
-							<Nav.Link as={NavLink} activeclassname="active" to="/profile">
-								Profile
-							</Nav.Link>
-						</Nav.Item>
-
-						<Nav.Item>
-							<Nav.Link as={NavLink} activeclassname="active" to="/">
-								Log out
-							</Nav.Link>
-						</Nav.Item>
+								<Nav.Item>
+									<Nav.Link as={NavLink} activeclassname="active" to="/" onClick={logout}>
+										Log out
+									</Nav.Link>
+								</Nav.Item>
+							</React.Fragment>
+						)}
 					</Nav>
 				</Navbar.Collapse>
 			</Navbar>
 		</div>
 	);
-}
+};
 
 export default NavBar;
