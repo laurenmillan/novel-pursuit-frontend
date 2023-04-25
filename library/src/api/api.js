68198f1df0dacdad99dc215ef535c720157a7862
Lauren Millan <laurenmillan@mydsn.net>
2023-04-25 09:56:43 -0400

    Comment out token

--- library/src/api/api.js
+++ library/src/api/api.js
@@ -126,7 +126,7 @@ class LibraryApi {
 export default LibraryApi;
 
 // for now, put token ("testuser" / "password" on class)
-LibraryApi.token =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ' +
-	'SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0.' +
-	'FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc';
+// LibraryApi.token =
+// 	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ' +
+// 	'SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0.' +
+// 	'FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc';
