092d2f4981bf0e7a7024cfd5ee632b7864a98f96
Lauren Millan <laurenmillan@mydsn.net>
2023-04-26 08:09:39 -0400

    Add code for bookmarks feature

--- library/src/App.js
+++ library/src/App.js
@@ -3,14 +3,13 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import LibraryApi from './api/api';
 
 import './App.css';
-import Home from './Components/Home';
 import Login from './Components/LoginForm';
 import Signup from './Components/SignupForm';
 import Profile from './Components/Profile';
 import Bookmarks from './Components/Bookmarks';
 import Main from './Components/Main';
 import NavBar from './Components/NavBar';
-import './Components/style.css'
+import './Components/style.css';
 import jwt_decode from 'jwt-decode';
 
 /** Library App logic. 
@@ -18,16 +17,19 @@ import jwt_decode from 'jwt-decode';
  * - signup and logout functions use the LibraryApi to make requests to the backend API. If successful, they update 
  *    the token and store it in the browser's local storage.
  * - This maintains the user's session and avoids the need for the user to login when they revisit the website.
+ * -useEffect will run whenever the token value changes to ensure that the user information is fetched and
+ *    updated based on the changes to the token.
+ * 
 */
 
 const App = () => {
-  const [ currentUser, setCurrentUser ] = useState(null);
-  const [token, setToken] = useState(localStorage.getItem('token')) // retrieve the value of the token key from LS
-  LibraryApi.token=token
+	const [ currentUser, setCurrentUser ] = useState(null);
+	const [ token, setToken ] = useState(localStorage.getItem('token')); // retrieve the value of the token key from LS
+	LibraryApi.token = token;
 
-  console.debug('App', 'currentUser=', currentUser, 'token=', token);
+	console.debug('App', 'currentUser=', currentUser, 'token=', token);
 
-  useEffect(
+	useEffect(
 		() => {
 			async function fetchUser() {
 				if (LibraryApi.token) {
@@ -47,7 +49,7 @@ const App = () => {
 		[ token ]
 	); // Add token as a dependency
 
-  async function signup(signupData) {
+	async function signup(signupData) {
 		try {
 			const token = await LibraryApi.signup(signupData);
 			localStorage.setItem('token', token);
@@ -60,36 +62,46 @@ const App = () => {
 		}
 	}
 
-	async function login(loginData){
-		try{
-			const token = await LibraryApi.login(loginData)
-			localStorage.setItem('token', token)
-      setToken(token)
-      LibraryApi.token = token
-      return {success:true}
-		}catch(error){
-      console.error('Failed to login:', error)
-      return {success: false, error}
+	async function login(loginData) {
+		try {
+			const token = await LibraryApi.login(loginData);
+			localStorage.setItem('token', token);
+			setToken(token);
+			LibraryApi.token = token;
+			return { success: true };
+		} catch (error) {
+			console.error('Failed to login:', error);
+			return { success: false, error };
 		}
 	}
 
-  /** Logout by clearing app's state, remove user's auth token from browser's LS */
+	/** Logout by clearing app's state, remove user's auth token from browser's LS */
 	function logout() {
 		setCurrentUser(null);
 		setToken(null);
 		localStorage.removeItem('token');
 	}
 
+	async function bookmarks(username, id) {
+		if (currentUser) {
+			try {
+			} catch (error) {
+				console.error('Failed to bookmark this book', error);
+			}
+		} else {
+			console.error('User must be logged in to bookmark a book.');
+		}
+	}
+
 	return (
 		<div className="App">
 			<NavBar user={token} logout={logout} />
-			{/* <Main /> */}
 			<Routes>
-				<Route exact path="/" element={<><Home /><Main user={currentUser} /></>} />
+				<Route exact path="/" element={<Main user={currentUser} />} />
 				<Route exact path="/login" element={<Login login={login} />} />
 				<Route exact path="/signup" element={<Signup signup={signup} />} />
 				<Route exact path="/profile" element={<Profile user={currentUser} setCurrentUser={setCurrentUser} />} />
-				<Route exact path="/bookmarks" element={<Bookmarks />} />
+				<Route exact path="/bookmarks" element={<Bookmarks bookmarks={bookmarks} />} />
 				<Route path="/*" element={<Navigate to="/" />} />
 			</Routes>
 		</div>
