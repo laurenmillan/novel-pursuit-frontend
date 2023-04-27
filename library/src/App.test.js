7a7689249b7765602508b376debfdc84a0d539e4
Lauren Millan <laurenmillan@mydsn.net>
2023-04-27 18:20:55 -0400

    Add test environments

--- library/src/App.test.js
+++ library/src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders', () => {
+	render(
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	);
 });
