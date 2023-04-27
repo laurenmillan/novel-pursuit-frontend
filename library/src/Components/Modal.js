04ecdcb6ae7bc4df4a8b7e8f92b04233f692c610
Lauren Millan <laurenmillan@mydsn.net>
2023-04-27 11:59:31 -0400

    Add updated layout and styles

--- library/src/Components/Modal.js
+++ library/src/Components/Modal.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
-import { faBook, faBookmark } from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faBook, faBookmark } from '@fortawesome/free-solid-svg-icons';
 
-/** Renders book modal.
- * 
- * - Displays book information for specific book selected by user.
- * - User can exit the modal by either clicking the X button or click outside the modal.
- * - Component limits the displayed authors and publishers to the first element.
- * - Allows a user to save a book to /bookmarks by clicking the bookmark button.
- * 
- */
-
-const Modal = ({ show, item, closeModal, bookmarks }) => {
-	console.debug('bookmarks=', bookmarks);
-
+const Modal = ({ show, item, closeModal }) => {
 	if (!show) {
 		return null;
 	}
@@ -23,15 +11,9 @@ const Modal = ({ show, item, closeModal, bookmarks }) => {
 	const publishers = item.publisher ? item.publisher.slice(0, 1) : ''; // Limit results to the first publisher
 	const authors = item.author_name ? item.author_name.slice(0, 1) : ''; // Limit results to the first author
 
-	const saveBook = () => {
-		const isbn = item.isbn && item.isbn.length > 0 ? item.isbn[0].toString() : null;
-
-		if (isbn) {
-			bookmarks(isbn); // Call the bookmarks function with the book ISBN when the Save Book button is clicked
-			closeModal();
-		} else {
-			console.error('ISBN not found');
-		}
+	const handleButtonClick = (evt) => {
+		evt.stopPropagation();
+		console.log('Bookmark button clicked');
 	};
 
 	return (
@@ -43,25 +25,26 @@ const Modal = ({ show, item, closeModal, bookmarks }) => {
 					</button>
 					<div className="inner-box">
 						{coverUrl ? (
-							<img src={coverUrl} alt={item.title} />
+							<img className="book-cover" src={coverUrl} alt={item.title} />
 						) : (
-							<FontAwesomeIcon icon={faBook} size="6x" style={{ marginRight: '15' }} />
+							<FontAwesomeIcon className="book-placeholder" icon={faBook} size="6x" />
 						)}
 						<div className="info">
 							<h1 className="title">{item.title}</h1>
 							<h3 className="author">by {authors}</h3>
 							<h5 className="publisher">
-								Publisher: <br />
-								{publishers} <br />
+								Publisher: {publishers}
 								<span className="publish-date">
-									Publish Date: <br />
-									{item.first_publish_year}
+									&nbsp;|&nbsp;Publish Date: {item.first_publish_year}
 								</span>
 							</h5>
-						</div>
-						<div>
-							<button className="bookmarks" onClick={saveBook}>
-								<FontAwesomeIcon icon={faBookmark} size="1x" />
+							<button className="bookmark-btn" type="submit" onClick={handleButtonClick}>
+								<span className="bookmark-text">
+									<FontAwesomeIcon className="bookmark-icon" icon={faBookmark} size="1x" />
+									<span className="add-to-bookmarks" style={{ fontSize: '0.8rem' }}>
+										&nbsp;Add to Bookmarks
+									</span>
+								</span>
 							</button>
 						</div>
 						<br />
