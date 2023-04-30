40f28f83a2069c7315ce6944ef7ae97d678932ff
Lauren Millan <laurenmillan@mydsn.net>
2023-04-30 12:49:24 -0400

    Add testing

--- library/src/App.test.js
+++ library/src/App.test.js
@@ -1,6 +1,6 @@
+import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import React from 'react';
 import App from './App';
 
 test('renders', () => {
--- library/src/api/api.js
+++ library/src/api/api.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+const axios = require('axios');
 
 const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3001';
 const LIBRARY_SEARCH_URL = 'https://openlibrary.org/search.json';
