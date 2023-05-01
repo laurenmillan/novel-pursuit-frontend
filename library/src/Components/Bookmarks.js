dad0d3e0b786ddf8751c1318ac4433cb987a6aa7
Lauren Millan <laurenmillan@mydsn.net>
2023-05-01 19:20:50 -0400

    Add styles

--- library/src/Components/Bookmarks.js
+++ library/src/Components/Bookmarks.js
@@ -1,15 +1,67 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookmark, faBook } from '@fortawesome/free-solid-svg-icons';
+import { useAppContext } from './Context/AppContext';
+import './style.css';
 
-/** Renders a Bookmarks page. 
- * 
- * -Displays saved "bookmarks" for logged in user.
- * 
-*/
+/** Renders a Bookmarks page.
+ *
+ * - Displays saved books "bookmarks" for logged in user.
+ *
+ */
 
 const Bookmarks = () => {
+	const { favorites, removeFromFavorites } = useAppContext();
+
+	console.log('favorites are', favorites);
+
 	return (
-		<div>
+		<div className="bookmarks">
 			<h1>Bookmarks</h1>
+			<div className="bookmarks-grid">
+				{favorites.length > 0 ? (
+					favorites.map((item) => {
+						const coverUrl = item.cover_i
+							? `https://covers.openlibrary.org/b/id/${item.cover_i}-M.jpg`
+							: null;
+						const publisher = item.publisher ? item.publisher.slice(0, 1) : '(Information Unavailable)'; // Limit results to the first publisher
+						const author = item.author_name ? item.author_name.slice(0, 1) : '(Information Unavailable)'; // Limit results to the first author
+
+						return (
+							<div key={item.key} className="book-item">
+								{coverUrl ? (
+									<img className="book-cover" src={coverUrl} alt={item.title} />
+								) : (
+									<FontAwesomeIcon className="book-placeholder" icon={faBook} size="6x" />
+								)}
+								<div className="info">
+									<h1 className="title">{item.title}</h1>
+									<h3 className="author">by {author}</h3>
+									<h5 className="publisher">
+										Publisher: {publisher}
+										<span className="publish-date">
+											&nbsp;|&nbsp;Publish Date: {item.first_publish_year}
+										</span>
+									</h5>
+								</div>
+								<button
+									className="bookmark-btn"
+									type="submit"
+									onClick={(e) => {
+										e.stopPropagation();
+										removeFromFavorites(item.key);
+									}}
+								>
+									<FontAwesomeIcon className="bookmark-icon" icon={faBookmark} size="1x" />
+									<span className="add-to-bookmarks">&nbsp;Remove from Bookmarks</span>
+								</button>
+							</div>
+						);
+					})
+				) : (
+					<span className="no-bookmarks-message">You don't have any books that are bookmarked yet!</span>
+				)}
+			</div>
 		</div>
 	);
 };
